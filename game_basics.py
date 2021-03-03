2ef8fe197c3f3a1733157eeb23b7c501d4a2b982
keew13 <padamwark@gmail.com>
2021-03-03 15:52:37 +0530

    Minor changes in game basics

--- game_basics.py
+++ game_basics.py
@@ -12,21 +12,6 @@ class BoardIndexNotInRange(Exception):
     def __str__(self):
         return self.message+f"x:{self.x}, y:{self.y} --> Valid ranges for x and y are between [1, 3] and [1, 3]."
 
-class BoardPositionOccupied(Exception):
-    """
-        BoardPositionOccupied is an exception raised when player tries
-        to put his marker on an already occupied position.
-    """
-    def __init__(self, x, y, board):
-        self.message = "Position already occupied.\n"
-        self.x = x
-        self.y = y
-        self.board = board
-        super().__init__(self.message)
-    
-    def __str__(self):
-        return self.message+f"Board at position [{self.x}, {self.y}] already occupied by {self.board[self.x-1][self.y-1]}."
-
 class TicTacToe:
     """
         TicTacToe class provides the basic board structure and utility functions
@@ -41,7 +26,6 @@ class TicTacToe:
                 None
         """
         self.board = [["", "", ""], ["", "", ""], ["", "", ""]]
-        self.winner = ""
 
     def reset_board(self):
         """
@@ -69,6 +53,25 @@ class TicTacToe:
                     if(i!=2):
                         print("-"*50)
     
+    def check_empty(self, x, y):
+        """
+            check_empty checks if the given position on the board is empty
+            or not.
+
+            Parameter(s):
+                x (integer): the x position of the cell,
+                             range: 1<=X<=3
+                y (integer): the y position of the cell,
+                             range: 1<=y<=3
+
+            Return(s):
+                1: the given position is empty
+                0: the given position is occupied
+        """
+        if self.board[x-1][y-1]=="":
+            return 1
+        return 0
+
     def make_move(self, x, y, player):
         """
             make_move allows to the player to put his marker on the
@@ -84,36 +87,50 @@ class TicTacToe:
         """
         if((x<1 or x>3) or (y<1 or y>3)):
             raise BoardIndexNotInRange(x, y)
-        elif(self.board[x-1][y-1]==""):
-            self.board[x-1][y-1]=player
-        elif(self.board[x-1][y-1]=="X" or self.board[x-1][y-1]=="O"):
-            raise BoardPositionOccupied(x, y, self.board)
+        elif(self.check_empty(x, y)):
+            self.board[x-1][y-1] = player
 
-    def is_win(self, player):
+    def is_win(self):
         """
             is_win checks if any player has won given the current status of the board.
             The checks are made row wise, column wise and diagonally. If three of the 
             current player's marker line up then it wins.
         
             Parameter(s):
-                player (string): marker of the current player
-                                 accepted value: [X, O]
+                None
+
+            Return(s):
+                "X": Player X has won
+                "O": Player O has won
+                "Draw": Match has drawn
+                "Match Ongoing": Match is in play
         """
-        win = [player]*3
-        if self.winner == "":
-            for i in range(0, 3):
-                temp = self.board[i]
-                if temp == win:
-                    self.winner = player
-                    break
-        if self.winner == "":
-            for j in range(0, 3):
-                temp = [self.board[i][j] for i in range(0, 3)] 
-                if temp == win:
-                    self.winner = player
-                    break
-        if self.winner == "":
-            temp1 = [self.board[i][i] for i in range(0, 3)]
-            temp2 = [self.board[i][2-i] for i in range(0, 3)]
-            if(temp1 == win or temp2 == win):
-                self.winner = player
+        win_X = ["X"]*3
+        win_O = ["O"]*3
+
+        for i in range(0, 3):
+            temp = self.board[i]
+            if temp == win_X:
+                return "X"
+            elif temp == win_O:
+                return "O"
+
+        for j in range(0, 3):
+            temp = [self.board[i][j] for i in range(0, 3)] 
+            if temp == win_X:
+                return "X"
+            elif temp == win_O:
+                return "O"
+
+        temp1 = [self.board[i][i] for i in range(0, 3)]
+        temp2 = [self.board[i][2-i] for i in range(0, 3)]
+        if(temp1 == win_X or temp2 == win_X):
+            return "X"
+        elif(temp1 == win_O or temp2 == win_O):
+            return "O"
+        
+        for i in range(1, 4):
+            for j in range(1, 4):
+                if(self.check_empty(i, j)==1):
+                    return "Match Ongoing"        
+        return "Draw"
--- play.py
+++ play.py
@@ -1,32 +1,35 @@
-from game_basics import *
+from mini_max import *
 
-b = TicTacToe()
+winner = ""
+b = Approach1()
 b.display_board()
 for i in range(0, 9):
     if i%2==0:
         print("Player 1's Turn")
+        print(b.best_move("X", "O", "X", 1, i, i))
         x = int(input("row pos: "))
         y = int(input("col pos: "))
         b.make_move(x, y, "X")
         b.display_board()
-        b.is_win("X")
-        if b.winner == "X":
+        winner = b.is_win()
+        if winner == "X":
             print("Player 1 has won.")
             b.reset_board()
             break
     else:
         print("Player 2's Turn")
+        print(b.best_move("O", "X", "O", 1, i, i))
         x = int(input("row pos: "))
         y = int(input("col pos: "))
         b.make_move(x, y, "O")
         b.display_board()
-        b.is_win("O")
-        if b.winner == "O":
+        winner = b.is_win()
+        if winner == "O":
             print("Player 2 has won.")
             b.reset_board()
             break
 
-if b.winner == "":
+if b.is_win() == "Draw":
     print("Match has drawn.")
     b.reset_board()
 
